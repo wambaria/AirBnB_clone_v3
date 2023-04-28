0fcac3713d075d0adde8a58937c72a124981635f
laban254 <labanrotich6544@gmail.com>
2023-04-28 14:58:46 +0300

    Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""My flask file"""
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def clean_up(exception=None):
+    """eliminates current Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, threaded=True)
