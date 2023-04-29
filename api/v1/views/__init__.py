b1e3135b6e79715632ecad663b541375cb1d1ee3
Francis Onyach <franblog7@gmail.com>
2023-04-29 13:18:58 -0700

    correct pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,11 +2,13 @@
 
 """init file for views module"""
 
-from api.v1.views.users import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.states import *
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
